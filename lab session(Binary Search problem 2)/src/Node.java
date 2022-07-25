2499fd6c58de3b241b5667aeef0f17f80d4efac9
Connectgauravkaushik <101702222+Connectgauravkaushik@users.noreply.github.com>
2022-07-25 22:48:37 +0530

    Add files via upload

---
+++ lab session(Binary Search problem 2)/src/Node.java
@@ -0,0 +1,12 @@
+
+public class Node {
+// node structure
+int nodeData; // consider as key value
+Node leftNode, rightNode;
+
+Node(int data){
+	nodeData =data;
+	leftNode = rightNode = null;
+    }
+
+ }
